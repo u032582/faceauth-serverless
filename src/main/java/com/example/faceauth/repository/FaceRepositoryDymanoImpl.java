14e4a13668e121ba1632899717eef2cca1511255
u032582 <u032582@gmail.com>
2022-03-02 23:29:17 +0900

    native版リリース候補

--- src/main/java/com/example/faceauth/repository/FaceRepositoryDymanoImpl.java
+++ src/main/java/com/example/faceauth/repository/FaceRepositoryDymanoImpl.java
@@ -1,6 +1,9 @@
 package com.example.faceauth.repository;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
@@ -25,8 +28,10 @@ import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
 import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
 import software.amazon.awssdk.services.dynamodb.model.DescribeTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
 import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
 import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
 import software.amazon.awssdk.services.dynamodb.model.TableDescription;
@@ -63,8 +68,66 @@ public class FaceRepositoryDymanoImpl implements FaceRepository, InitializingBea
 		ddb.deleteItem(deleteReq);
 	}
 
+	/**
+	 * {@link #findByAccountIdByEnhanced(String)} がnative版でエラーを吐くので低レベルAPIで対応
+	 * 
+	 */
 	@Override
 	public Face findByAccountId(String accountId) {
+		HashMap<String, AttributeValue> keyToGet = new HashMap<String, AttributeValue>();
+
+		keyToGet.put("accountId", AttributeValue.builder().s(accountId).build());
+		GetItemRequest request = GetItemRequest.builder().key(keyToGet).tableName(tableName).build();
+
+		{
+			Map<String, AttributeValue> returnedItem = ddb.getItem(request).item();
+
+			if (returnedItem != null) {
+				Set<String> keys = returnedItem.keySet();
+				Face ret = new Face();
+				for (String attr : keys) {
+					switch (attr) {
+					case "accountId":
+						ret.setAccountId(returnedItem.get("accountId").s());
+						break;
+					case "faceId":
+						ret.setFaceId(returnedItem.get("faceId").s());
+						break;
+					case "faceImage":
+						ret.setFaceImage(returnedItem.get("faceImage").s());
+						break;
+					case "imageFormat":
+						ret.setImageFormat(returnedItem.get("imageFormat").s());
+						break;
+					case "boundingLeft":
+						ret.setBoundingLeft(Double.parseDouble(returnedItem.get("boundingLeft").n()));
+						break;
+					case "boundingTop":
+						ret.setBoundingTop(Double.parseDouble(returnedItem.get("boundingTop").n()));
+						break;
+					case "boundingWidth":
+						ret.setBoundingWidth(Double.parseDouble(returnedItem.get("boundingWidth").n()));
+						break;
+					case "boundingHeight":
+						ret.setBoundingHeight(Double.parseDouble(returnedItem.get("boundingHeight").n()));
+						break;
+					case "created":
+						ret.setCreated(LocalDateTime.parse(returnedItem.get("created").s()));
+						break;
+					}
+				}
+				if (ret.getAccountId() == null)
+					throw ResourceNotFoundException.builder().message("accountId[" + accountId + "] face not found")
+							.build();
+				return ret;
+			} else {
+				log.warn("No item found with the key %s!\n", accountId);
+			}
+		}
+		return null;
+	}
+
+	public Face findByAccountIdByEnhanced(String accountId) {
 		DynamoDbTable<Face> mappedTable = dynamodbClient.table(tableName, TableSchema.fromBean(Face.class));
 		Key key = Key.builder().partitionValue(accountId).build();
 		var ret = mappedTable.getItem(r -> r.key(key));
@@ -73,8 +136,28 @@ public class FaceRepositoryDymanoImpl implements FaceRepository, InitializingBea
 		return ret;
 	}
 
+	/**
+	 * {@link #saveByEnhanced(Face)} がnative版でエラーを吐くので低レベルAPIで対応
+	 * 
+	 */
 	@Override
 	public void save(Face face) {
+		HashMap<String, AttributeValue> itemValues = new HashMap<String, AttributeValue>();
+		itemValues.put("accountId", AttributeValue.builder().s(face.getAccountId()).build());
+		itemValues.put("faceId", AttributeValue.builder().s(face.getFaceId()).build());
+		itemValues.put("faceImage", AttributeValue.builder().s(face.getFaceImage()).build());
+		itemValues.put("imageFormat", AttributeValue.builder().s(face.getImageFormat()).build());
+		itemValues.put("created", AttributeValue.builder().s(face.getCreated().toString()).build());
+		itemValues.put("boundingLeft", AttributeValue.builder().n(Double.toString(face.getBoundingLeft())).build());
+		itemValues.put("boundingTop", AttributeValue.builder().n(Double.toString(face.getBoundingTop())).build());
+		itemValues.put("boundingWidth", AttributeValue.builder().n(Double.toString(face.getBoundingWidth())).build());
+		itemValues.put("boundingHeight", AttributeValue.builder().n(Double.toString(face.getBoundingHeight())).build());
+
+		PutItemRequest request = PutItemRequest.builder().tableName(tableName).item(itemValues).build();
+		ddb.putItem(request);
+	}
+
+	public void saveByEnhanced(Face face) {
 		DynamoDbTable<Face> mappedTable = dynamodbClient.table(tableName, TableSchema.fromBean(Face.class));
 		mappedTable.putItem(face);
 	}
